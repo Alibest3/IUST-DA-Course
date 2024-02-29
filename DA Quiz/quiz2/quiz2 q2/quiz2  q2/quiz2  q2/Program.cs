82ff3f3da1054a1535c6cfa1f90bf3d96d996eba
Alibest3 <alibest3@yahoo.com>
2024-02-29 14:20:36 +0330

    AD quiz 2 Added

---
+++ DA Quiz/quiz2/quiz2 q2/quiz2  q2/quiz2  q2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz2__q2
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int n = int.Parse(input[0]);
+            int k = int.Parse(input[1]);
+
+            int[,] dp = new int[n + 1, k + 1];
+
+            for (int i = 0; i <= n; i++)
+            {
+                dp[i, 0] = 0;
+                dp[i, 1] = i;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 2; j <= k; j++)
+                {
+                    dp[i, j] = int.MaxValue;
+
+                    int low = 1;
+                    int high = i;
+                    while (low <= high)
+                    {
+                        int mid = (low + high) / 2;
+
+                        int tossesLeft = Math.Max(dp[mid - 1, j - 1], dp[i - mid, j]);
+
+                        dp[i, j] = Math.Min(dp[i, j], tossesLeft + 1);
+
+                        if (dp[mid - 1, j - 1] < dp[i - mid, j])
+                        {
+                            low = mid + 1;
+                        }
+                        else
+                        {
+                            high = mid - 1;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(dp[n,k]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DA Quiz/quiz2/quiz2/quiz2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string first = Console.ReadLine();
+                string second = Console.ReadLine();
+
+                int firstIndex = 0;
+                int secondIndex = 0;
+
+                while (firstIndex < first.Length && secondIndex < second.Length)
+                {
+                    if (first[firstIndex] == second[secondIndex])
+                    {
+                        firstIndex++;
+                        secondIndex++;
+                    }
+                    else
+                    {
+                        firstIndex++;
+                    }
+                }
+                if (secondIndex != second.Length)
+                {
+                    string s = string.Empty;
+                    for (int j = second.Length - 1; j > -1; j--)
+                    {
+                        s += second[j];
+                    }
+                    second = s;
+                    firstIndex = 0;
+                    secondIndex = 0;
+                    while (firstIndex < first.Length && secondIndex < second.Length)
+                    {
+                        if (first[firstIndex] == second[secondIndex])
+                        {
+                            firstIndex++;
+                            secondIndex++;
+                        }
+                        else
+                        {
+                            firstIndex++;
+                        }
+                    }
+
+                }
+
+                if (secondIndex == second.Length)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
