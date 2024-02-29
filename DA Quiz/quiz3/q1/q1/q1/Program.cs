aa4538374dfa497197052ee299e9d2d1c1d9a84f
Alibest3 <alibest3@yahoo.com>
2024-02-29 14:20:42 +0330

    AD quiz 3 Added

---
+++ DA Quiz/quiz3/q1/q1/q1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int[,] coordinates = new int[n, 2];
+
+            for (int i = 0; i < n;i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int x = int.Parse(input[0]);
+                int y = int.Parse(input[1]);
+
+                coordinates[i, 0] = x;
+                coordinates[i, 1] = y;
+
+            }
+
+            int[] xarray = new int[n];
+            int[] yarray = new int[n];
+
+            for (int i = 0; i<n;i++)
+            {
+                for (int j = 0;j < 1;j++)
+                {
+                    xarray[i] = j;
+                    yarray[i] = j;
+                }
+
+            }
+            
+            for (int i = 0; i < n;i++)
+            {
+                   
+            }
+            
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
---
+++ DA Quiz/quiz3/q2/q1_2/q1_2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        List<int> x_coords = new List<int>();
+        List<int> y_coords = new List<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int x = int.Parse(s[0]);
+            int y = int.Parse(s[1]);
+            x_coords.Add(x);
+            y_coords.Add(y);
+        }
+
+        int max_x = x_coords.Max();
+        int min_x = x_coords.Min();
+        int max_y = y_coords.Max();
+        int min_y = y_coords.Min();
+
+        int width = max_x - min_x;
+        int height = max_y - min_y;
+
+        int num_teppeh = (width > height) ? width : height;
+
+        Console.WriteLine(num_teppeh - (n - 1));
+    }
+}
+
